caaca5599a675af21e246cfcd5b56f87e3f898a6
jianghong <jianghong2020@qq.com>
2023-10-11 15:36:38 +0800

    链接钱包

--- src/MyBtn.tsx
+++ src/MyBtn.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import WebApp from "@twa-dev/sdk";
+import { TonConnectButton } from "@tonconnect/ui-react";
 
 export default function MyBtn() {
     const [show, setShow] = useState(0);
     return (
         <>
-
+            <TonConnectButton />
             <button
                 onClick={() => {
                     setShow(show > 0 ? 0 : 1);
