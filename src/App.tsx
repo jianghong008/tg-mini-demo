fc5c1980c477176eaff5c2aeee977af6ef032854
jianghong <jianghong2020@qq.com>
2023-10-12 17:06:03 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ import Wallet from './Wallet'
 import WebApp from '@twa-dev/sdk';
 WebApp.ready();
 function App() {
-
   return (
     <TonConnectUIProvider manifestUrl='https://jianghong008.github.io/tg-mini-demo/tonconnect-manifest.json' uiPreferences={{theme:THEME.DARK}}>
-      <h3>Telegram Mini</h3>
       <div className="card">
         <Wallet/>
       </div>
--- src/Wallet.tsx
+++ src/Wallet.tsx
@@ -1,28 +1,42 @@
 import { useState } from "react";
 import WebApp from "@twa-dev/sdk";
-import { TonConnectButton,useTonAddress,useTonWallet } from "@tonconnect/ui-react";
+import {
+    TonConnectButton,
+    useTonAddress,
+    useTonWallet,
+    useTonConnectUI,
+    SendTransactionRequest
+} from "@tonconnect/ui-react";
+
+const transaction:SendTransactionRequest = {
+    validUntil: Math.floor(Date.now() / 1000) + 360,
+    messages: [
+        {
+            address: 'EQAZdaS8oCaAaWx0FYrae8k7YrvHpAGyUCF8r2q7hKqKJX1t',
+            amount: '0.05',
+        }
+    ]
+};
 
 export default function MyBtn() {
     const [show, setShow] = useState(0);
-    const address = useTonAddress()
-    const wallet = useTonWallet()
-    
+    const address = useTonAddress();
+    const wallet = useTonWallet();
+    const [tonConnectUI, setOptions] = useTonConnectUI()
     return (
         <div className="flex justify-center items-center flex-col gap-4">
-            <h4>{wallet?.device.platform}</h4>
-            <p className=" text-ellipsis whitespace-nowrap w-40 overflow-hidden">address:{address?address:'Not Connected'}</p>
+            <h4>{wallet?.device.appName}</h4>
             <TonConnectButton />
             <button
                 onClick={() => {
                     setShow(show > 0 ? 0 : 1);
                     !show && WebApp.MainButton.show();
                     show && WebApp.MainButton.hide();
-                  
                 }}
             >
                 {show ? "Hide" : "Show"} Button
             </button>
-            <button></button>
+            <button onClick={()=>tonConnectUI.sendTransaction(transaction)}>send</button>
         </div>
     );
 }
