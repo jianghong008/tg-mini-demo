4637cd5a7999d3b68ca0378d41b061a1b8a5d893
jianghong <jianghong2020@qq.com>
2023-10-13 17:39:30 +0800

    交易成功

--- src/Wallet.tsx
+++ src/Wallet.tsx
@@ -4,15 +4,19 @@ import {
     TonConnectButton,
     useTonWallet,
     useTonConnectUI,
-    SendTransactionRequest
+    SendTransactionRequest,
+    CHAIN,
 } from "@tonconnect/ui-react";
 
-const transaction:SendTransactionRequest = {
+const amount = 0.1
+
+const transaction: SendTransactionRequest = {
     validUntil: Math.floor(Date.now() / 1000) + 360,
+    network: CHAIN.TESTNET,
     messages: [
         {
             address: 'EQAZdaS8oCaAaWx0FYrae8k7YrvHpAGyUCF8r2q7hKqKJX1t',
-            amount: '0.05',
+            amount: (amount * Math.pow(10, 9)) + '',
         }
     ]
 };
@@ -20,21 +24,34 @@ const transaction:SendTransactionRequest = {
 export default function MyBtn() {
     const [show, setShow] = useState(0);
     const wallet = useTonWallet();
-    const [tonConnectUI, ] = useTonConnectUI()
+    const [tonConnectUI,] = useTonConnectUI()
+
+    WebApp.MainButton.setText('主按钮')
+
+    const send = () => {
+        if (!tonConnectUI.connected) {
+            WebApp.showAlert('请先连接')
+            return
+        }
+        tonConnectUI.sendTransaction(transaction)
+    }
+    const setBtn = () => {
+        setShow(show > 0 ? 0 : 1);
+        !show && WebApp.MainButton.show();
+        show && WebApp.MainButton.hide();
+    }
     return (
         <div className="flex justify-center items-center flex-col gap-4">
             <h4>{wallet?.device.appName}</h4>
             <TonConnectButton />
-            <button
-                onClick={() => {
-                    setShow(show > 0 ? 0 : 1);
-                    !show && WebApp.MainButton.show();
-                    show && WebApp.MainButton.hide();
-                }}
-            >
-                {show ? "Hide" : "Show"} Button
+            <button onClick={setBtn }>
+                {show ? "Hide" : "Show"} Bottom Button
             </button>
-            <button onClick={()=>tonConnectUI.sendTransaction(transaction)}>send</button>
+            {tonConnectUI.connected && (
+                <button onClick={send}>Send {amount} Ton</button>
+            )}
+
+
         </div>
     );
 }
